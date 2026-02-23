90f0917011ae889052b3db03859887ad8455e2f2
RareChimera87 <sanctireinadiaz@gmail.com>
2026-02-22 19:39:39 -0500

    Waybar setup

--- hypr/.config/hypr/scripts/handle_workspaces.sh
+++ hypr/.config/hypr/scripts/handle_workspaces.sh
@@ -5,6 +5,7 @@ sleep 3
 handle() {
   if hyprctl monitor | grep -q "DP-1"; then
     hyprctl dispatch moveworkspacetomonitor 1 DP-1
+    notify-send "Pantalla DP-1 conectada"
   else
     hyprctl dispatch moveworkspacetomonitor 1 eDP-1
   fi
